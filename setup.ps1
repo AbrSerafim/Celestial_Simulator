3d73f8558747aa66e57e9439388ea2362477b57e
AbrSerafim <eabreuserafim@gmail.com>
2026-09-21 17:49:07 -0300

    Wrote README.md and updated files

--- setup.ps1
+++ setup.ps1
@@ -1,6 +1,6 @@
 Write-Host "Setting up project..."
 
-winget install Python.Python.3.9
+winget install Python.Python.3.9 --accept-package-agreements --accept-source-agreements --disable-interactivity
 py -3.9 -m venv .venv
 .\.venv\Scripts\Activate.ps1
 python -m pip install --upgrade pip
