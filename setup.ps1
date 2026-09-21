e79df27b80fd404db81a5228c138eecf444b2c10
ERICH ABREU SERAFIM <erich.serafim@maua.br>
2026-09-21 11:46:25 -0300

    Added setup script

---
+++ setup.ps1
@@ -0,0 +1,9 @@
+Write-Host "Setting up project..."
+
+winget install Python.Python.3.9
+py -3.9 -m venv .venv
+.\.venv\Scripts\Activate.ps1
+python -m pip install --upgrade pip
+pip install -r requirements.txt
+
+Write-Host "Done!"
